b207c7e0277d7a47dd94d3faac4d7ed4b41dd24b
Ananya Jana <ananya.jana@gmail.com>
2017-11-09 15:58:49 +0530

    incorrect version

--- src/max_subarray_sum.cpp
+++ src/max_subarray_sum.cpp
@@ -9,8 +9,8 @@ Author:Ananya Jana
 
 int main()
 {
-	int N, K, T, max_sum, temp;
-	N = K = T = max_sum = temp = 0;
+	int N, K, T, max_sum, temp, i, j;
+	N = K = T = max_sum = temp = i = j = 0;
 	int * arr = NULL;	// dynamic array to hold the array elements
 	
 	
@@ -28,17 +28,23 @@ int main()
 	   		scanf("%d", &arr[n]); // scanning the elements of the array one by one
 		}
 		max_sum = arr[0];
-		for(int i = 0; i < N; ++i){
-			for(int j = i; j < N; ++j){
+		for(i = 0; i < N; ++i){
+		printf("1.temp = %d,max_sum = %d, i = %d\n", temp, max_sum, i);
+			for(j = i; j < N; ++j){
 				temp += arr[j];
 				if(temp > max_sum)
 					max_sum = temp;
 				else if(max_sum > 0 && temp < 0 && j < N - 1){
 					i = j+1;
 					temp = 0;
+					printf("2.temp = %d,max_sum = %d\n", temp, max_sum);
 					continue;
 				}
+				printf("3.temp = %d,max_sum = %d, i = %d, j = %d\n", temp, max_sum, i, j);
 			}
+			
+			if(j == N)
+				break;
 		}
 		printf("max_sum = %d\n", max_sum);
 		if(arr)
