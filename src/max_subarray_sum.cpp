f59d0e661bdc4874ece94158c20e38459419ef21
Ananya Jana <ananya.jana@gmail.com>
2017-11-09 15:36:09 +0530

    incorrect output

--- src/max_subarray_sum.cpp
+++ src/max_subarray_sum.cpp
@@ -9,9 +9,9 @@ Author:Ananya Jana
 
 int main()
 {
-	int N, K, T;
-	N = K = T = 0;
-	char * arr = NULL;	// dynamic array to hold the array elements
+	int N, K, T, max_sum, temp;
+	N = K = T = max_sum = temp = 0;
+	int * arr = NULL;	// dynamic array to hold the array elements
 	
 	
     
@@ -19,16 +19,28 @@ int main()
 
 	for(int t = 1; t <= T; ++t){
 		scanf("%d", &N); // scanning the number of elements in the array
-		arr = (char*)malloc(N * sizeof(char));
+		arr = (int*)malloc(N * sizeof(int));
 		if(NULL == arr){
 			printf("couldn't allocate space! Exiting\n");
 			exit(1);
 		}
 		for(int n = 0; n < N; ++n){
-	   		scanf("%d ", &arr[n]); // scanning the elements of the array one by one
-		
+	   		scanf("%d", &arr[n]); // scanning the elements of the array one by one
 		}
-		
+		max_sum = arr[0];
+		for(int i = 0; i < N; ++i){
+			for(int j = i; j < N; ++j){
+				temp += arr[j];
+				if(temp > max_sum)
+					max_sum = temp;
+				else if(max_sum > 0 && temp < 0 && j < N - 1){
+					i = j+1;
+					temp = 0;
+					continue;
+				}
+			}
+		}
+		printf("max_sum = %d\n", max_sum);
 		if(arr)
 			free(arr);
 	}
