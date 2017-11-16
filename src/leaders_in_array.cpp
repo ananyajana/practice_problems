1346b0d811d30c7a387ab9ecd8985bb5f37b0fd9
Ananya Jana <ananya.jana@gmail.com>
2017-11-16 18:40:25 +0530

    initial check-in

--- src/leaders_in_array.cpp
+++ src/leaders_in_array.cpp
@@ -8,18 +8,21 @@ Author:Ananya Jana
 
 int main()
 {
-	int N, K, T, max, i, j;
-	N = K = T = max = i = j = 0;
+	int N, K, T, last_leader_seen, i, j;
+	N = K = T = last_leader_seen = i = j = 0;
 	int * arr = NULL;	// dynamic array to hold the array elements
+	int * leads = NULL;	// dynamic array to hold the array leaders
 	
 	scanf("%d", &T);
 
 	for(int t = 1; t <= T; ++t){
+		last_leader_seen = i = j = 0;
 		
 		scanf("%d", &N); // scanning the number of elements in the array
 		arr = (int*)malloc(N * sizeof(int));
+		leads = (int*)calloc(N, sizeof(int));	// We need the initial leads array to be cleared
 		
-		if(NULL == arr){
+		if((NULL == arr) ||(NULL == leads)){
 			//printf("couldn't allocate space! Exiting\n");
 			exit(1);
 		}
@@ -27,7 +30,7 @@ int main()
 	   		scanf("%d", &arr[i]); // scanning the elements of the array one by one
 		}
 		
-		for(i = 1; i < N; ++i){
+		for(i = N; i > 0; --i){	//start the search for leaders from the rightmost end of the array
 
 		}
 		
