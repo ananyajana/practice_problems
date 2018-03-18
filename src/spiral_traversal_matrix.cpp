45919fe4f85cf03251e39a87c696a9c6fe2ec5e2
Ananya Jana <ananya.jana@gmail.com>
2018-03-18 08:12:01 +0530

    generic version for R*C matrix
    Signed-off-by: Ananya Jana <ananya.jana@gmail.com>

--- src/spiral_traversal_matrix.cpp
+++ src/spiral_traversal_matrix.cpp
@@ -1,4 +1,4 @@
-/*  Spirally traversing a matrix 4*4
+/*  Spirally traversing a generic matrix R*C
 
 Author:Ananya Jana
 */
@@ -6,33 +6,45 @@ Author:Ananya Jana
 #include <stdio.h>
 #include <stdlib.h>
 
-#define	MATRIX_SZ	16
+#define	MATRIX_SZ	4
 
 
 int main()
 {
-	int M, N, T, i, j;
-	N = T = i = 0;
-	int *a;	// dynamic array to hold the array elements, the highest elements to the left and right
-	a = NULL;
-	
+	int M, N, T, i, j, increase, k, l, m, n;
+	N = T = i = increase = 0;
+	int a[MATRIX_SZ][MATRIX_SZ];	// dynamic array to hold the array elements, the highest elements to the left and right
 	
 	scanf("%d", &T);
 
 	for(int t = 1; t <= T; ++t){		
-		a = (int*)malloc(MATRIX_SZ * sizeof(int));
-		
-		
 		for(i = 0; i < MATRIX_SZ; ++i){
-	   		scanf("%d", &a[i]); // scanning the elements of the array one by one
+			for(j = 0; j < MATRIX_SZ; ++j)
+	   			scanf("%d", &a[i][j]); // scanning the elements of the array one by one
 		}
 		
+		k = l = 0;
+		m = n = MATRIX_SZ;
 		
-		
-		//freeing up the dynamically allocated memory
-		if(a1)
-			free(a1);
-		
+		while((k < m) && (l < n)){		
+			for(i = 0; i < n; ++i)
+				printf("%d ", a[k][i]);
+				//printf("a[%d][%d] = %d \n", i, j, a[i][j]);
+			++k;
+			for(i = k; i  < m; ++i)
+				printf("%d ", a[i][m - 1]);
+				//printf("a[%d][%d] = %d \n", i, j, a[i][j]);
+			--m;
+			for(i = m - 1; i >= l; --i)
+				printf("%d ", a[n - 1][i]);
+				//printf("a[%d][%d] = %d \n", i, j, a[i][j]);
+			--n;
+			for(i = n - 1; i > k; --i)
+				printf("%d ", a[i][l]);
+				//printf("a[%d][%d] = %d \n", i, j, a[i][j]);
+			++l;
+		}
+		printf("\n");
 	}
 }
 
