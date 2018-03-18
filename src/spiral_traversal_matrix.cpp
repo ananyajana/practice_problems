c4d745f5c1810ce376877a38d215fe9083db70b9
Ananya Jana <ananya.jana@gmail.com>
2018-03-18 08:29:54 +0530

    generic version for R*C matrix
    Signed-off-by: Ananya Jana <ananya.jana@gmail.com>

--- src/spiral_traversal_matrix.cpp
+++ src/spiral_traversal_matrix.cpp
@@ -6,43 +6,54 @@ Author:Ananya Jana
 #include <stdio.h>
 #include <stdlib.h>
 
-#define	MATRIX_SZ	4
+#define	R		4
+#define	C		4
 
 
 int main()
 {
 	int M, N, T, i, j, increase, k, l, m, n;
 	N = T = i = increase = 0;
-	int a[MATRIX_SZ][MATRIX_SZ];	// dynamic array to hold the array elements, the highest elements to the left and right
-	
+	int a[R][C];
 	scanf("%d", &T);
 
 	for(int t = 1; t <= T; ++t){		
-		for(i = 0; i < MATRIX_SZ; ++i){
-			for(j = 0; j < MATRIX_SZ; ++j)
+		for(i = 0; i < R; ++i){
+			for(j = 0; j < C; ++j)
 	   			scanf("%d", &a[i][j]); // scanning the elements of the array one by one
 		}
 		
 		k = l = 0;
-		m = n = MATRIX_SZ;
-		
-		while((k < m) && (l < n)){		
-			for(i = 0; i < n; ++i)
+		m = R;
+		n = C;
+		while((k < m) && (l < n)){
+			// print the first row from the remaining matrix		
+			for(i = l; i < n; ++i)
 				printf("%d ", a[k][i]);
 				//printf("a[%d][%d] = %d \n", i, j, a[i][j]);
 			++k;
+			
+			// print the last column from the remaining matrix
 			for(i = k; i  < m; ++i)
-				printf("%d ", a[i][m - 1]);
-				//printf("a[%d][%d] = %d \n", i, j, a[i][j]);
-			--m;
-			for(i = m - 1; i >= l; --i)
-				printf("%d ", a[n - 1][i]);
+				printf("%d ", a[i][n - 1]);
 				//printf("a[%d][%d] = %d \n", i, j, a[i][j]);
 			--n;
-			for(i = n - 1; i > k; --i)
-				printf("%d ", a[i][l]);
-				//printf("a[%d][%d] = %d \n", i, j, a[i][j]);
-			++l;
+			
+			// print the last row from the remaining matrix
+			if ( k < m){
+				for(i = n - 1; i >= l; --i)
+					printf("%d ", a[m - 1][i]);
+					//printf("a[%d][%d] = %d \n", i, j, a[i][j]);
+				--m;
+			}
+			
+			// print the first column from the remaining matrix
+			if (l < n){
+				for(i = m - 1; i >= k; --i)
+					printf("%d ", a[i][l]);
+					//printf("a[%d][%d] = %d \n", i, j, a[i][j]);
+				++l;
+			}
 		}
 		printf("\n");
 	}
