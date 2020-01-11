97542a7dcb59e0cdf3545b2ee6d148de55496cbf
Ananya Jana <ananya.jana@gmail.com>
2020-01-11 11:19:47 -0500

    working version

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -8,29 +8,28 @@ for t in range(T):
 
 
 def longest_common_prefix(s, n):
-    print('the strings are :', s)
-    print('the number of strings :', n)
     match_idx = -1
     s1 = s[0]
-    s2 = s[1]
-    print('s1 is :', s1)
-    print('s2 is :', s2)
-    print('length of s1 is :', len(s1))
     for i in range(len(s1)):
         for k in range(1, n):
             if i < len(s[k]):
                 if s1[i] != s[k][i]:
                     # return the string till previous match_idx or else -1
-                    return match_idx if match_idx < 0 else s1[:match_idx]
+                    return match_idx if match_idx < 0 else s1[:match_idx + 1]
+                #print(' i here :', i)
+            else:
+                # return the string till previous match_idx or else -1
+                return match_idx if match_idx < 0 else s1[:match_idx + 1]
 
         # if we have reached here, all the strings are checked for the particular
         # index i and they match so far
         if k == n - 1:
-            print('match_idx is :', match_idx)
+            #print(' i here2 :', i)
             match_idx = i
+            #print('match_idx here :', match_idx)
 
     # done with checking all the strings for a common prefix    
-    return s1[:match_idx] 
+    return s1[:match_idx + 1] 
     
 
 for t in range(T):
