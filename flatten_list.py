c2cb2555b54622155c348349626c5f7935185ab4
Ananya Jana <ananya.jana@gmail.com>
2020-01-23 16:22:34 -0500

    checking in just the node class changes

---
+++ flatten_list.py
@@ -0,0 +1,53 @@
+# problems from geeksforgeeks must do coding questions
+
+T = int(input())
+sz_list =[]
+N_list = []
+lists = []
+for t in range(T):
+    N_list.append(int(input()))
+    sz_list.append(input())
+    lists.append(input())
+
+class Node:
+    # fn to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.right = None
+        self.bottom = None
+
+#linked list contains a node object
+class linked_list:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.bottom
+
+def create_linked_list(s, n):
+    data_list = []
+    data_list = s.split()
+    #print(data_list)
+    #print(s)
+    llist = linked_list()
+    first = Node(data_list[0])
+    llist.head = new_node = first
+    #new_node = new_node.next
+    for i in range(1, n):
+        new_node.bottom = Node(int(data_list[i]))
+        new_node = new_node.bottom
+
+    #llist.print_list()
+    return llist
+
+
+for t in range(T):
+    #s1 = input()
+    sz = sz_list[t]
+    n = N_list[t]
+    x = X_list[t]
+    llist = create_linked_list(s1, n)
+    #llist.print_list()
