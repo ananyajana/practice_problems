88091250fb331b59221305e6d133d529ce869c01
Ananya Jana <ananya.jana@gmail.com>
2020-02-05 12:45:57 -0500

    making stack using linked list

---
+++ linked_list_stack.py
@@ -0,0 +1,38 @@
+# problems from geeksforgeeks must do coding questions
+
+T = int(input())
+st_list =[]
+N_list = []
+for t in range(T):
+    N_list.append(int(input()))
+    st_list.append(input())
+
+class Node:
+    # fn to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class MyStack:
+    def __init__(self):
+        self.front = None
+
+    # method to add an item to the queue
+    def push(self, item):
+        node = Node(item)
+        if self.front is not None:
+            node.next = self.front
+        self.front = node
+    
+    # method to remove an item from the queue
+    def pop(self):
+        if self.front is None:
+            return -1
+        else:
+            temp = self.front
+            self.front = self.front.next
+            return temp.data
+    
+for t in range(T):
+    s1 = st_list[t]
+    n = N_list[t]
