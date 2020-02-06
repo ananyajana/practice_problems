5bad6009127cf3e21b184efab6ee013a10bda2bd
Ananya Jana <ananya.jana@gmail.com>
2020-02-06 00:42:16 -0500

    checking in framework

---
+++ sort_0_1_2.py
@@ -0,0 +1,45 @@
+# problems from geeksforgeeks must do coding questions
+
+T = int(input())
+st_list =[]
+N_list = []
+for t in range(T):
+    N_list.append(int(input()))
+    st_list.append(input())
+
+class Node:
+    # fn to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+#linked list contains a node object
+class linked_list:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+
+def create_linked_list(s, n):
+    data_list = []
+    data_list = s.split()
+    llist = linked_list()
+    first = Node(int(data_list[0]))
+    llist.head = new_node = first
+    for i in range(1, n):
+        new_node.next = Node(int(data_list[i]))
+        new_node = new_node.next
+
+    return llist
+
+
+for t in range(T):
+    #s1 = input()
+    s1 = st_list[t]
+    n = N_list[t]
+    llist = create_linked_list(s1, n)
+    llist.print_list()
