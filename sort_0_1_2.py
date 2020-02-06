894af2493c58636f7ac38caab6b00f89eafa8232
Ananya Jana <ananya.jana@gmail.com>
2020-02-06 00:56:31 -0500

    incomplete wip

--- sort_0_1_2.py
+++ sort_0_1_2.py
@@ -37,6 +37,20 @@ def create_linked_list(s, n):
     return llist
 
 
+def sort_0_1_2(head):
+    temp = start = head
+    last = prev = new_head = None
+
+	
+    while temp.next is not None:
+        temp = temp.next
+    #set the last pointer to the last node of the linked list
+    last = temp
+    # the linked list is traversed, whenever '0' node is found, it is
+    # appended at the beginning and whenever '2' node is found, it is
+    # appended at the end
+
+
 for t in range(T):
     #s1 = input()
     s1 = st_list[t]
