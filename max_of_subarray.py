cfad9c1f7de86e116da20af25f00a3b4f3e9c50f
Ananya Jana <ananya.jana@gmail.com>
2020-02-19 06:21:40 -0500

    initial framework for i/o format

--- max_of_subarray.py
+++ max_of_subarray.py
@@ -1,6 +1,6 @@
 # to find the max of subarrays, is this similar to
 # finding the minimum element of a stack at any time?
-
+import numpy as np
 T = int(input())
 st_list =[]
 N_list = []
@@ -14,10 +14,16 @@ def find_max_of_subarrays(s, n):
     k = int(k)
     s = s.split()
     s = [int(s[i]) for i in range(len(s))]
-    print(s)
+    #print(s)
     print(n)
     print(k)
-    return -1
+    s = np.array(s)
+    print(s)
+    max_vals = []
+    for i in range(len(s)):
+        sub_arr = s[i: i + k]
+        max_vals.append(sub_arr.max())
+    return max_vals
     
 for t in range(T):
     s1 = st_list[t]
@@ -25,4 +31,7 @@ for t in range(T):
     #print(n)
     #print(s1)
     
-    print(find_max_of_subarrays(s1, n))
+    max_arr = find_max_of_subarrays(s1, n)
+    if len(max_arr) != 0:
+	    for i in range(len(max_arr)):
+                print(max_arr[i])
