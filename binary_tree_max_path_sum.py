ff8e0200e75de6dfe22eae5fbf60dbd60c7662c9
Ananya Jana <ananya.jana@gmail.com>
2020-05-02 12:08:12 -0400

    this code does not handle negative data

--- binary_tree_max_path_sum.py
+++ binary_tree_max_path_sum.py
@@ -33,13 +33,29 @@ def create_binary_tree(i):
         root.right = create_binary_tree(2*i + 2)
         return root
 
+# assumes all numbers are non-negative
 def max_path_sum(root):
-    left_path_len = find_path_len(root.left)
-    right_path_len = find_path_len(root.right)
-    if left_path_len > right_path_len:
-        return left_path_len + root.data
+    _, max_sum = find_path_len(root)
+    return max_sum
+
+def find_path_len(root):
+    if root is None:
+        return [0, 0]
+    left_path_len, left_max_sum = find_path_len(root.left)
+    right_path_len, right_max_sum = find_path_len(root.right)
+    node_max_sum = 0
+    max_path_len = 0
+    if left_path_len >= right_path_len:
+        max_path_len = left_path_len + root.data
     else:
-        return right_path_len + root.data
+        max_path_len = right_path_len + root.data
+    node_max_sum = left_path_len + root.data + right_path_len
+    #cur_max = left_max_sum > right_max_sum? left_max_sum:right_max_sum
+    cur_max = left_max_sum if left_max_sum > right_max_sum else right_max_sum
+    if node_max_sum < cur_max:
+        node_max_sum = cur_max
+
+    return [max_path_len, node_max_sum]
 
 # code execution starts here
 if __name__=='__main__':
