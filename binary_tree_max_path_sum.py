6b5c48b4a551e1fee698099ba0b6c4ff434991d3
Ananya Jana <ananya.jana@gmail.com>
2020-05-01 22:21:31 -0400

    incomplete

---
+++ binary_tree_max_path_sum.py
@@ -0,0 +1,49 @@
+class Node:
+    # fn to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        self.next_right = None
+
+#linked list contains a node object
+class binary_tree:
+    def __init__(self):
+        self.root = None
+    
+    def print_tree_inorder(self, root):
+        if(root):
+            self.print_tree_inorder(root.left)
+            print(root.data)
+            self.print_tree_inorder(root.right)
+# recursive function to print the left view of a binary tree
+
+#arr=[20, 8, 22, 5, 3, 4, 25]
+arr=[1, 2, 3, 7, 6, 5, 4]
+
+# create a tree with the array from the given index
+# i.e. create a tree with the elements arr[i:n]
+def create_binary_tree(i):
+    global arr
+    if i >= len(arr):
+        return None
+    else:
+        root = Node(arr[i])
+        root.left = create_binary_tree(2*i + 1)
+        root.right = create_binary_tree(2*i + 2)
+        return root
+
+def max_path_sum(root):
+    left_path_len = find_path_len(root.left)
+    right_path_len = find_path_len(root.right)
+    if left_path_len > right_path_len:
+        return left_path_len + root.data
+    else:
+        return right_path_len + root.data
+
+# code execution starts here
+if __name__=='__main__':
+    # start with the empty list
+    bintree = binary_tree()
+    bintree.root = create_binary_tree(0)
+    print(max_path_sum(bintree.root))
