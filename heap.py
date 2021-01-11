ddb56a6a62b3dfe586465f12e7eaf2c3c40c33a8
Ananya Jana <ananya.jana@gmail.com>
2021-01-11 06:44:54 -0500

    max heap creation

--- heap.py
+++ heap.py
@@ -6,24 +6,26 @@ def heapify(arr, n, i):
     :param i: subtree rooted at ith index
     :return: 
     '''
+    print('n = ', n)
     # code here
     l = left(i)
     print('l = ', l)
     r = right(i)
     print('r = ', r)
-    if l < n and arr[l] > arr[i]:
+    if l <= n-1 and arr[l] > arr[i]:
         largest = l
     else:
         largest = i
-    if r < n and arr[r] > arr[largest]:
+    if r <= n-1 and arr[r] > arr[largest]:
         largest = r
     
     if largest != i:
+        print('swapping {} with {}'.format(arr[largest], arr[i]))
         temp = arr[largest]
-        arr[largest] = arr[k]
-        arr[k] = arr[largest]
+        arr[largest] = arr[i]
+        arr[i] = temp
     
-    heapify(arr, n, largest)
+        heapify(arr, n, largest)
 
 def buildHeap(arr,n):
     '''
@@ -32,7 +34,7 @@ def buildHeap(arr,n):
     :return: None
     '''
     # code here
-    for k in range((n - 1)//2 + 1, -1, -1):
+    for k in range((n - 1)//2 - 1, -1, -1):
         print('k = ', k)
         heapify(arr, n, k)
 
@@ -48,5 +50,7 @@ def parent(pos):
 
 
 arr = [4, 5, 6, 7, 8]
+#arr = [8, 7, 6, 5, 4]
 n = len(arr)
 buildHeap(arr,n)
+print(arr)
