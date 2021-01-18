54f42aa5f45d2d4b31780087a918bc53020244b1
Ananya Jana <ananya.jana@gmail.com>
2021-01-18 04:42:03 -0500

    initial version, might not work completely

--- kth_largest.py
+++ kth_largest.py
@@ -2,8 +2,10 @@
 # function to build min heap
 
 
-# need to maintain of maxheap or k elements.
-# whenever a next element comes the previous kth element needs to go out
+# need to maintain of minheap or k elements.
+# whenever a next element comes, the kth largest element is checked.
+# if it is smaller than the minimum element then ignored. Otherwise
+# the minheap is poppoed and the new element inserted
 T = int(input())
 st_list =[]
 for t in range(T):
@@ -208,3 +210,17 @@ print('the min elements extracted is:', extractMin())
 print(arr)
 print('the min elements extracted is:', extractMin())
 print(arr)
+
+kth_max_heap = []
+
+arr = [8, 7, 6, 5, 4, 10, 16, 2, 1, 3]
+i = 0
+while i < k-1:
+    print('-1')
+    heapq.heappush(kth_max_elem, arr[i])
+    i += 1
+
+for i in range(k-1, n):
+    if arr[i] > kth_max_heap[0]:
+        print(heapq.heappushpop(kth_max_heap, arr[i]))
+
