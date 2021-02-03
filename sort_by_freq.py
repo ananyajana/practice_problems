290003e7e61fc3ecd1d1aeb03cfb0590e2475ec4
Ananya Jana <ananya.jana@gmail.com>
2021-02-03 09:24:08 -0500

    working version, but squeezes the input

--- sort_by_freq.py
+++ sort_by_freq.py
@@ -11,7 +11,8 @@ def sort_by_freq(a, n):
     temp_arr = [-999] * n
     freq_arr =[0] * n
 
-    print('hello ', a)
+    #print('hello ', a)
+    last_idx = 0
     for i in range(n):
         for j in range(n):
             # our goal is to find an empty slot in the temp_arr
@@ -28,11 +29,44 @@ def sort_by_freq(a, n):
             if temp_arr[j] == -999 or a[i] == temp_arr[j]:
                 temp_arr[j] = a[i]
                 freq_arr[j] += 1
+                if last_idx < j:
+                    last_idx = j
                 break
             # else visit the next slot in temp_arr to see if it is empty
 
-    print('temp_arr: ', temp_arr)
-    print('freq_arr: ', freq_arr)
+    #print('temp_arr: ', temp_arr)
+    #print('last_idx in temp_arr: ', last_idx)
+    #print('freq_arr: ', freq_arr)
+
+    m = last_idx + 1
+    for i in range(m):
+        for j in range(i+1, m):
+            # The big frequencies should be towards the front(descending order)
+            if freq_arr[i] < freq_arr[j]:
+                # exchange the frequencies
+                temp1 = freq_arr[i]
+                freq_arr[i] = freq_arr[j]
+                freq_arr[j] = temp1
+
+                # exchange the actual elements
+                temp1 = temp_arr[i]
+                temp_arr[i] = temp_arr[j]
+                temp_arr[j] = temp1
+            # if the frequencies are equal then smaller element would
+            # be at the front
+            if freq_arr[i] == freq_arr[j] and temp_arr[i] > temp_arr[j]:
+                # exchange the actual elements
+                temp1 = temp_arr[i]
+                temp_arr[i] = temp_arr[j]
+                temp_arr[j] = temp1
+                
+    #print('temp_arr: ', temp_arr)
+    #print('last_idx in temp_arr: ', last_idx)
+    #print('freq_arr: ', freq_arr)
+
+    final_arr = temp_arr[:m]
+    #print('final_arr: ', final_arr)
+    return final_arr
 
 for t in range(T):
     s = st_list[t]
