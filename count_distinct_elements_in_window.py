6b8fa3393994e04486ddd2afde2d9b533466cf4a
Ananya Jana <ananya.jana@gmail.com>
2021-02-16 19:50:49 -0500

    working version

---
+++ count_distinct_elements_in_window.py
@@ -0,0 +1,27 @@
+T = int(input())
+
+# find all unique quadruples from the given array that sums up to the nuique number
+# the elements need not be contiguous
+
+def count_distinct(arr, n, k):
+    res = []
+    for i in range(n-4):
+        counts = dict()
+        for j in range(k):
+            if arr[i+j] not in counts.keys():
+                counts[arr[i+j]] = 1
+            else:
+                counts[arr[i+j]] += 1
+        print('counts:', counts)
+        res.append(len(counts))
+    return res
+    
+
+for t in range(T):
+    n = int(input())
+    k = int(input())
+    arr = list(map(int, input().strip().split()))
+    print('arr: {}, n: {}, k: {}'.format(arr, n, k))
+    print(count_distinct(arr, n, k))
+
+
