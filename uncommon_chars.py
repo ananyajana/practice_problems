453518e5b27c558fc946754724a0383f8291262d
Ananya Jana <ananya.jana@gmail.com>
2021-03-05 09:08:56 -0500

    fixed: alphabetical order

--- uncommon_chars.py
+++ uncommon_chars.py
@@ -1,5 +1,5 @@
-import operator
-from collections import OrderedDict
+#import operator
+#from collections import OrderedDict
 
 T = int(input())
 
@@ -8,7 +8,8 @@ T = int(input())
 # the value is -1, else the value corresponding to the keys is 1
 
 def uncommon(arr1, arr2):
-    temp = OrderedDict()
+    #temp = OrderedDict()
+    temp = dict()
     for i in range(len(arr1)):
         temp[arr1[i]] = 1 # common element in second array is not yet found
     for j in range(len(arr2)):
@@ -16,32 +17,18 @@ def uncommon(arr1, arr2):
             temp[arr2[j]] = -1  # common element in second array is found
         else:
             temp[arr2[j]] = 1 # common element was not found in the first array
-    #temp_copy = temp.copy()
-    #temp_copy = {k:v for k, v in temp.items() if v != -1}
-    '''
-    for key in temp_copy.keys():
-        if temp_copy[key] == -1:  # the match is found, we can delete the corresponding key:
-            del temp_copy[arr2[j]]
-    for k,v in temp_copy.items():
-        if v == -1:
-           del temp_copy[k]
-    '''
+ 
+    print(sorted(temp))
+    print('list form: ', list(sorted(temp.items())))
 
     str1 = ''
-    #for key in temp_copy.keys():
-    #for k,v in temp_copy.items():
-    #for k,v in temp.items():
-    for key in temp.keys():
+    for key in sorted(temp.keys()):
         if temp[key] != -1:
-            #if v != -1:
-            #str1 += k
             str1 += key
 
-    #if not temp_copy:
     if not temp:
         return -1
     else:
-        #return temp_copy
         return str1
 
 for t in range(T):
